d0e45dfa98cefae1c45bc56b73c3b98817b95010
AnikDev <anikshahrieroishik@gmail.com>
2023-11-22 10:42:35 +0600

    T-shirt added

---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber';
+import {easing} from 'maath';
+import { useSnapshot } from 'valtio';
+import state from '../store';
+
+
+const CameraRig = ({children}) => {
+    const group = useRef();
+    const snap = useSnapshot(state);
+    useFrame((state, delta) =>{
+        const isBreakPoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <= 600;
+
+        
+        //set the intial position of the model
+        let targetPositon = [-0.4, 0, 2];
+        if(snap.intro){
+            if(isBreakPoint) targetPositon=[0,0,2];
+            if(isMobile) targetPositon =[0, 0.2, 2.5];
+            }
+            else{
+                if(isMobile) targetPositon =[0,0,2.5];
+                else targetPositon =[0,0,2];
+            }
+        
+        //set model camera position 
+        easing.damp3(state.camera.position, targetPositon, 0.25, delta)
+
+        //set model rotation easily 
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y/10, -state.pointer.x/5,0],
+            0.25,
+            delta
+        )
+    })
+
+   
+  return <group ref={group}>{children}</group>
+}
+
+export default CameraRig
